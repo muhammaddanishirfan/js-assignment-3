48d92b2f366cda39a554ad2a48018eaf2147ba09
muhammaddanishirfan <danishanwer2008@gmail.com>
2024-09-13 16:41:25 +0500

    Add files via upload

---
+++ Assignment 3/app.js
@@ -0,0 +1,181 @@
+// chap 9-11 Q-1
+
+// var city = prompt("Where Do You Live")
+
+// if("city == karachi")
+//     {
+//     alert("Wellcome To City Of Lights")
+// }
+
+// chap 9-11 Q-2
+
+// var gender = prompt("Gender")
+
+// if(gender === "male"){
+//     alert("Welcome Sir")
+// }else if(gender === "female"){
+//     alert("Welcome Ma'am")
+// }
+
+// chap 9-11 Q-3
+
+// var signal = prompt("Color Of Road Traffic Signal")
+
+// if (signal === "red") {
+//     alert("Must Stop")
+// } else if (signal === "yellow") {
+//     alert("Ready To Move")
+// } else if (signal === "green") {
+//     alert("Move Now")
+// }
+
+// chap 9-11 Q-4
+
+// var fuel = +prompt("Remaining Fuel In Car (In Liter)")
+
+// if (fuel < "0.25")
+//     {
+//         alert("Please refill the fuel in your car")
+//     }
+
+
+// chap 9-11 Q-5
+
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+//  if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+// chap 9-11 Q-6
+
+// var sub1 = +prompt("enter marks of eng")
+
+// var sub2 = +prompt("enter marks of urdu")
+
+// var sub3 = +prompt("enter marks of maths")
+
+// var totalmarks = 300
+// var obtmarks = sub1 + sub2 + sub3
+
+// var percentage = obtmarks / totalmarks * 100
+
+// var grade;
+// var remarks;
+// if (percentage >= 80) {
+//     grade = "A+", remarks = "Excellent"
+// } else if (percentage >= 70) {
+//     grade = "A", remarks = "Good"
+// }
+// else if (percentage >= 60) {
+//     grade = "B", remarks = "Need to improve"
+// }
+// else if (percentage >= 50) {
+//     grade = "C", remarks = "Sorry your fail"
+// } else {
+//     grade = "Fail", remarks = "Repeat again"
+// }
+
+// document.write("<h1>Marks Sheet </h1>  Total Marks:"+totalmarks ," <br/> Marks obtained:" +obtmarks,"<br/> Percentage:" +percentage, "<br/> Grade:" +grade , "<br/> Remarks:" +remarks, )
+
+// chap 9-11 Q-7
+
+// var game = +prompt("Guess The Secret Number (From 1 to 10)")
+
+// if  (game === 7)
+//     {
+//         alert("Bingo! Correct answer.")   
+//     } else if(game === 7-1 || game === 7+1)
+//         {
+//         alert("Close enough to the correct answer")
+//     }
+
+// chap 9-11 Q-8
+
+// var anynumber = +prompt("Enter any number")
+
+// if(anynumber % 3 == 0)
+//     {
+//         alert("Your Number is divisible by 3")
+//     }
+
+//     else {
+
+//         alert("Your Number is not divisible by 3")
+//     }
+
+// chap 9-11 Q-9
+
+// var anynumber = +prompt("Enter any number")
+
+// if(anynumber % 2 == 0){
+//     alert("your number is even")
+// }
+
+// else{
+//     alert("Your Number is odd")
+// }
+
+// chap 9-11 Q-10
+
+// var temp = +prompt("Enter temperature in C")
+
+// if(temp > 40){
+//     alert("Temperature is too hot")
+// }else if(temp > 30){
+//     alert("Temperature is normal")
+// }else if(temp > 20){
+//     alert("Temperature is cool")
+// }else if(temp > 10){
+//     alert("Temperature is too cold")
+// }
+
+// chap 9-11 Q-11
+
+// var num1 = +prompt("Enter 1st number to calculate")
+// var num2 = +prompt("Enter 2nd number to calculate")
+// var operator = prompt("Enter operator to calculate")
+
+// if (operator == "+") {
+//     alert(num1 + num2)
+// } else if (operator == "-") {
+//     alert(num1 - num2)
+// }else if (operator == "*") {
+//     alert(num1 * num2)
+// }else if (operator == "/"){
+//     alert(num1 / num2)
+// }else if (operator == "%") {
+//     alert(num1 % num2)
+// }
+
